4aad8ee3f025444dec51f5a5f8996e38f3c82f2f
Tiago <tiagocorx@gmail.com>
2020-07-29 21:56:47 -0300

    Updates

---
+++ precache-manifest.898216c9246ec25a3258f8ed0d3e30ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804ac754ca799460189e806cbd628770",
+    "url": "/To_Do_List_React/index.html"
+  },
+  {
+    "revision": "e133a2ee4689da174dba",
+    "url": "/To_Do_List_React/static/css/main.a98f48e8.chunk.css"
+  },
+  {
+    "revision": "e7427694aa197f53391e",
+    "url": "/To_Do_List_React/static/js/2.04e0af57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To_Do_List_React/static/js/2.04e0af57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e133a2ee4689da174dba",
+    "url": "/To_Do_List_React/static/js/main.9752d93b.chunk.js"
+  },
+  {
+    "revision": "e5195292a4dee494ff25",
+    "url": "/To_Do_List_React/static/js/runtime-main.14460867.js"
+  }
+]);
